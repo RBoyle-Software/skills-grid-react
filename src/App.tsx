6d1dd548e8fac71385a5d99a31f0ad1b4c204a49
Reland Boyle <reland.boyle@gmail.com>
2022-02-20 00:26:07 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SkillsBoard from './SkillsBoard.tsx'
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <TopNav /> */}
+      <SkillsBoard />
+      {/* <UserInput /> */}
+    </div>
+  );
+}
+
+export default App;

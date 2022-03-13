c3cbe7033b8c2a8ac29047e2f6c3f32fa0bbf312
Reland Boyle <reland.boyle@gmail.com>
2022-03-13 00:23:17 -0500

    Proxy, handleStatusToggle

--- src/SkillsBoard.jsx
+++ src/SkillsBoard.jsx
@@ -1,32 +1,28 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/SkillsBoard.css'
 
 
 function SkillsBoard() {
 
   const [skills, setSkills] = useState([]);
-  const [isAcquired, setIsAcquired] = useState('');
-  const statusRef = useRef('');
 
   useEffect(() => {
-    fetch('http://localhost:3100/user-skills')
+    fetch('/user-skills')
     .then(response => response.json())
     .then(data => setSkills(data))
   }, []);
 
-  function handleStatusToggle (el) {
-    console.log(el);
-    el.classList.toggle('acquired');
-    el.classList.toggle('outstanding');
+  const handleStatusToggle = (e) => {
+    e.target.classList.toggle('outstanding');
+    e.target.classList.toggle('acquired');
   }
-
 
   const skillsList = skills.map((skill, index) => {
     return (
       <div
         key={`Box #${index}`}
         className={`boxes ${skill.status}`}
-        onClick={(target) => handleStatusToggle(target)}
+        onClick={(e) => handleStatusToggle(e)}
       >
         {skill.value}
       </div>
